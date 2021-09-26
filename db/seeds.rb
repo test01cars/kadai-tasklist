6e74926f774dfc89d1966f13991398d9245af4e8
test.cars <test.cars3388@gmail.com>
2021-09-26 10:27:32 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..20).each do |number|
+  Task.create(status: '未実施', content: 'test task ' + number.to_s)
+end
